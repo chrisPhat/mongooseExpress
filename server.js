ee86849b516e9f65fe265232e34c275110247815
chrisPhat <chris@wearephat.uk>
2020-03-03 21:10:38 +0000

    add connection string

--- server.js
+++ server.js
@@ -1,15 +1,25 @@
 var express = require('express');
 var app = express();
 
-app.get('/', (req, res)=>{
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/testCollection', {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+})
+    .then(() => {
+        console.log('connected')
+    });
+
+app.get('/', (req, res) => {
     res.send('This is the Home page');
 });
 
-app.get('/about', (req, res)=>{
+app.get('/about', (req, res) => {
     res.send('This is the about Page');
 })
 
 //Listening for requests on port 3000
-app.listen(3000, ()=> {
+app.listen(3000, () => {
     console.log('Server listening on port 3000');
 })
