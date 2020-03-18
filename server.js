7fc2f9beab1b12ac6a3de473a70a07e5979e5405
chrisPhat <chris@wearephat.uk>
2020-03-18 09:22:26 +0000

    added comments

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 var express = require('express');
 var app = express();
 
+
+//The mongoose module allows us to connect to our Database, and run functions such as save quite easily
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/testCollection', {
+//.connect() requires the URL for our database, in this example, the connection is being made to a local instance of MongoDB
+// We are also passing in some 'options' here (useUnifiedTopology and useNewUrlParser) as some functionality is changing in MongoDB
+//and this will avoid us receiving error messages in our console log.
+mongoose.connect('mongodb://localhost:27019/testCollection', {
     useUnifiedTopology: true,
     useNewUrlParser: true
 })
     .then(() => {
-        console.log('connected')
+        console.log('connected to DB')//Upon Successful connection, we are using a Javasctipt .then block here to give us a message in in our console 
+    })
+    .catch((err) => {
+        console.log('Not Connected to DB : ' + err);//Upon unuccessful connection, we are using a Javasctipt .catch block here to give us a message in in our console with err displayed so that we can see what the issue is.
     });
 
 app.get('/', (req, res) => {
